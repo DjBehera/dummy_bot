7fed5e6af552a43d06828ac7d76fb5c2f5506122
channelmindcraft <dibyaj46@gmail.com>
2024-07-06 11:25:58 +0530

    updated key words added

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def home():
     """
     Handles the base web traffic
     """
-    return "Hello, World!"
+    return "Hello, World! Updated from Dj"
 
 if __name__ == '__main__':
     app.run(debug=True,port=3000)
